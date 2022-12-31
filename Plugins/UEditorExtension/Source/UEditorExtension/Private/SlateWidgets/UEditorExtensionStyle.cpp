30efbdb7e2974edc13400bf093a94fe178cab045
mark-fernandez <9023627+mfernandez4@users.noreply.github.com>
2022-12-31 15:56:38 -0800

    Added comments to the UEditorExtensionStyle class.

--- Plugins/UEditorExtension/Source/UEditorExtension/Private/SlateWidgets/UEditorExtensionStyle.cpp
+++ Plugins/UEditorExtension/Source/UEditorExtension/Private/SlateWidgets/UEditorExtensionStyle.cpp
@@ -10,25 +10,34 @@ TSharedPtr< FSlateStyleSet > FUEditorExtensionStyle::StyleInstance = NULL;
 
 void FUEditorExtensionStyle::Initialize()
 {
+	// Only register once, check if the style set is already registered
 	if (!StyleInstance.IsValid())
 	{
+		// Create the style set
 		StyleInstance = Create();
+		// Register the style set
 		FSlateStyleRegistry::RegisterSlateStyle(*StyleInstance);
 	}
 }
 
 void FUEditorExtensionStyle::Shutdown()
 {
+	// Unregister the style set
 	FSlateStyleRegistry::UnRegisterSlateStyle(*StyleInstance);
+	// Ensure there are no references to the style set
 	ensure(StyleInstance.IsUnique());
+	// Reset the style set
 	StyleInstance.Reset();
 }
 
 FName FUEditorExtensionStyle::GetStyleSetName()
 {
-	static FName StyleSetName(TEXT("UEditorExtensionStyle"));
+	// Return the style set name
+	static FName StyleSetName( ExtensionStyleName );
 	return StyleSetName;
 }
+
+// define the styles for the style set
 #ifndef IMAGE_BRUSH
 #define IMAGE_BRUSH( RelativePath, ... ) FSlateImageBrush( Style->RootToContentDir( RelativePath, TEXT(".png") ), __VA_ARGS__ )
 #endif
@@ -45,19 +54,25 @@ FName FUEditorExtensionStyle::GetStyleSetName()
 #define OTF_FONT( RelativePath, ... ) FSlateFontInfo( Style->RootToContentDir( RelativePath, TEXT(".otf") ), __VA_ARGS__ )
 #endif
 
+// define the size of the icons
 const FVector2D Icon16x16(16.0f, 16.0f);
 const FVector2D Icon20x20(20.0f, 20.0f);
 const FVector2D Icon40x40(40.0f, 40.0f);
 const FVector2D Icon64x64(64.0f, 64.0f);
 const FVector2D Icon128x128(128.0f, 128.0f);
 
+
 TSharedRef< FSlateStyleSet > FUEditorExtensionStyle::Create()
 {
-	TSharedRef< FSlateStyleSet > Style = MakeShareable(new FSlateStyleSet("UEditorExtensionStyle"));
+	// Create the style set and make it a shared reference
+	TSharedRef< FSlateStyleSet > Style = MakeShareable(new FSlateStyleSet(ExtensionStyleName));
+	// Set the content root to the plugin's content directory
 	Style->SetContentRoot(IPluginManager::Get().FindPlugin("UEditorExtension")->GetBaseDir() / TEXT("Resources"));
 
+	// Style->Set, create a SlateBrush from an image path, and set the brush's resource name
 	Style->Set("UEditorExtension.PluginAction", new IMAGE_BRUSH(TEXT("ButtonIcon_128"), Icon128x128));
 
+	// return the created style set
 	return Style;
 }
 
@@ -69,13 +84,16 @@ TSharedRef< FSlateStyleSet > FUEditorExtensionStyle::Create()
 
 void FUEditorExtensionStyle::ReloadTextures()
 {
+	// Check if the style set is valid or initialized
 	if (FSlateApplication::IsInitialized())
 	{
+		// Reload the textures used by the style set
 		FSlateApplication::Get().GetRenderer()->ReloadTextureResources();
 	}
 }
 
 const ISlateStyle& FUEditorExtensionStyle::Get()
 {
+	// Return the style set instance
 	return *StyleInstance;
 }
--- Plugins/UEditorExtension/Source/UEditorExtension/Public/SlateWidgets/UEditorExtensionStyle.h
+++ Plugins/UEditorExtension/Source/UEditorExtension/Public/SlateWidgets/UEditorExtensionStyle.h
@@ -9,23 +9,40 @@ class FUEditorExtensionStyle
 {
 public:
 
+	/**
+	 * Initialize the style set. This should only be called once per module.
+	 * Creates the Slate Style, and registers the style set with the Slate Style Registry.
+	 */
 	static void Initialize();
 
+	/**
+	 * Shutdown the style set. Unregisters the style set and resets the singleton.
+	 */
 	static void Shutdown();
 
 	/** reloads textures used by slate renderer */
 	static void ReloadTextures();
 
-	/** @return The Slate style set for the Shooter game */
+	/** @return The Slate style set */
 	static const ISlateStyle& Get();
 
+	/** @returns the name of the style set. */
 	static FName GetStyleSetName();
 
 private:
 
+	/**
+	 * Creates the Slate style set.
+	 * @returns The new Slate style set.
+	 */
 	static TSharedRef< class FSlateStyleSet > Create();
+
+	inline static FName ExtensionStyleName = "UEditorExtensionStyle";
 
 private:
 
+	/**
+	 * The Slate style set for the EditorExtension module
+	 */
 	static TSharedPtr< class FSlateStyleSet > StyleInstance;
 };
