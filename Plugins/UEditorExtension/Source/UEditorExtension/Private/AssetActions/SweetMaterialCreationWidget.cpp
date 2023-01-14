823f66a4c7d7df8a3996a3cebbced020786220cd
mark-fernandez <9023627+mfernandez4@users.noreply.github.com>
2023-01-14 14:03:03 -0800

    Modified SweetMaterialCreationWidget class. Started implementing UI Widget and functions for processing texture data.

--- Plugins/UEditorExtension/Source/UEditorExtension/Private/AssetActions/SweetMaterialCreationWidget.cpp
+++ Plugins/UEditorExtension/Source/UEditorExtension/Private/AssetActions/SweetMaterialCreationWidget.cpp
@@ -2,4 +2,137 @@
 
 
 #include "AssetActions/SweetMaterialCreationWidget.h"
+#include "DebugHeader.h"
+#include "EditorAssetLibrary.h"
+#include "EditorUtilityLibrary.h"
 
+#pragma region MaterialCreationCore
+
+void USweetMaterialCreationWidget::CreateMaterialFromSelectedTextures()
+{
+	// check if user wants to create a material with a custom name
+	if ( bCustomMaterialName )
+	{
+		// check if user has entered a name
+		if ( MaterialName.IsEmpty() || MaterialName.Equals( TEXT("M_") ) )
+		{
+			DebugHeader::ShowMsgDialog( EAppMsgType::Ok, TEXT("Please enter a valid name.") );
+			return;
+		}
+	}
+
+	// Get selected textures
+	const TArray<FAssetData> SelectedAssets = UEditorUtilityLibrary::GetSelectedAssetData();
+	TArray<UTexture2D*> SelectedTextures; // array to store selected textures
+	FString MaterialPath; // path to store material
+	FString SelectedTexturePath; // path to store selected texture
+
+	// check if user has selected any textures
+	if( !ProcessSelectedData( SelectedAssets, SelectedTextures, SelectedTexturePath) ) return;
+
+	if ( CheckIfMaterialNameIsUsed( SelectedTexturePath, MaterialName ) ) return;
+	DebugHeader::Print( SelectedTexturePath, FColor::Cyan );
+	
+	
+}
+
+// Process selected data, will filter out non-textures and return false if no textures are selected
+bool USweetMaterialCreationWidget::ProcessSelectedData(const TArray<FAssetData>& SelectedAssets,
+	TArray<UTexture2D*>& OutSelectedTextures, FString& OutSelectedTexturePath)
+{
+	// check if user has selected any textures
+	if ( SelectedAssets.Num() ==0 )
+	{
+		// show error message
+		DebugHeader::ShowMsgDialog( EAppMsgType::Ok, TEXT("Please select at least one texture.") );
+		return false;
+	}
+
+	// bool check if bCustomMaterialName is set
+	bool bCustomMaterialNameSet = false;
+
+	// loop through selected assets
+	for ( const FAssetData& SelectedData : SelectedAssets )
+	{
+		// get UObject from selected asset
+		const UObject* SelectedAsset = SelectedData.GetAsset();
+
+		// check if selected asset is valid, if not, skip it
+		if ( !SelectedAsset ) continue;
+		
+		// check if selected asset is a texture
+		if ( !SelectedData.GetClass()->IsChildOf( UTexture2D::StaticClass() ) )
+		{
+			// show error message
+			DebugHeader::ShowMsgDialog( EAppMsgType::Ok, FString::Printf(TEXT("Please select only textures.\n%s is not a texture."), *SelectedAsset->GetName()) );
+			return false;
+		}
+		
+		// get texture
+		UTexture2D* SelectedTexture = Cast<UTexture2D>( SelectedData.GetAsset() );
+
+		// check if texture is valid, if not, skip it
+		if ( !SelectedTexture ) continue;
+		
+		// add texture to array
+		OutSelectedTextures.Add( SelectedTexture );
+
+		// check if texture path is empty, assign it if it is
+		// only need to do this once
+		if ( OutSelectedTexturePath.IsEmpty() )
+		{
+			// get texture path
+			OutSelectedTexturePath = SelectedData.PackagePath.ToString();
+		}
+
+		// check if bCustomMaterialName is set, if not, set it
+		// only need to do this once
+		if ( !bCustomMaterialName && !bCustomMaterialNameSet )
+		{
+			// get texture name
+			FString TextureName = SelectedTexture->GetName();
+
+			// check if texture name starts with "T_"
+			if ( TextureName.StartsWith( TEXT("T_") ) )
+			{
+				// remove "T_" from texture name
+				TextureName.RemoveFromStart( TEXT("T_") );
+			}
+
+			// add texture name to material name
+			MaterialName.Empty();
+			MaterialName.Append( FString::Printf(TEXT("M_%s"), *TextureName) );
+
+			// set bCustomMaterialNameSet to true
+			bCustomMaterialNameSet = true;
+		}
+	}
+
+	// check if user has selected less than one texture
+	if ( OutSelectedTextures.Num() < 1 )
+	{
+		// show error message
+		DebugHeader::ShowMsgDialog( EAppMsgType::Ok, TEXT("Please select at least one texture.") );
+		return false;
+	}
+	
+	return true;
+}
+
+bool USweetMaterialCreationWidget::CheckIfMaterialNameIsUsed(const FString& FolderPathToCheck, const FString& MaterialNameToCheck)
+{
+	TArray<FString> ExistingAssetPaths = UEditorAssetLibrary::ListAssets( FolderPathToCheck, false );
+
+	for ( const FString& ExistingAssetPath : ExistingAssetPaths )
+	{
+		const FString ExistingAssetName = FPaths::GetBaseFilename(ExistingAssetPath);
+		if ( ExistingAssetName.Equals(MaterialNameToCheck) )
+		{
+			DebugHeader::ShowMsgDialog( EAppMsgType::Ok, FString::Printf(TEXT("Material name is already used.\n%s"), *ExistingAssetPath) );
+			return true;
+		}
+	}
+	return false;
+}
+
+#pragma endregion MaterialCreationCore
--- Plugins/UEditorExtension/Source/UEditorExtension/Public/AssetActions/SweetMaterialCreationWidget.h
+++ Plugins/UEditorExtension/Source/UEditorExtension/Public/AssetActions/SweetMaterialCreationWidget.h
@@ -13,5 +13,50 @@ UCLASS()
 class UEDITOREXTENSION_API USweetMaterialCreationWidget : public UEditorUtilityWidget
 {
 	GENERATED_BODY()
+
+#pragma region MaterialCreationCore
+public:
+
+	/**
+	 * Create a new material from the selected textures.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Material Creation")
+	void CreateMaterialFromSelectedTextures();
+
+	/**
+	 * Boolean to check if the material will be created with a custom name.
+	 */
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Material Creation" )
+	bool bCustomMaterialName = true;
+	
+	/**
+	 * Material Name to create. If empty, the name will be generated automatically.
+	 */
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Material Creation", meta = ( EditCondition = "bCustomMaterialName" ) )
+	FString MaterialName = TEXT("M_");
+	
+protected:
+
+private:
+
+	/**
+	 * Process the selected data. Filters out textures to create a material.
+	 * @param SelectedAssets - The selected data.
+	 * @param OutSelectedTextures - The filtered textures.
+	 * @param OutSelectedTexturePath - The texture path for one of the filtered textures.
+	 * @returns True if the process was successful. False if none of the selected assets
+	 * were textures. 
+	 */
+	bool ProcessSelectedData( const TArray<FAssetData>& SelectedAssets, TArray<UTexture2D*>& OutSelectedTextures, FString& OutSelectedTexturePath );
+
+	/**
+	 * Checks if the MaterialName is not being used by another material. If it is, it will return true.
+	 * @param FolderPathToCheck - The folder path to check for the material.
+	 * @param MaterialNameToCheck - The name of the material to check.
+	 * @returns True if the material name is being used. False if it is not.
+	 */
+	bool CheckIfMaterialNameIsUsed( const FString& FolderPathToCheck, const FString& MaterialNameToCheck );
+
+#pragma endregion
 	
 };
